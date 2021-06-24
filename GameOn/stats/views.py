18fd971e232b515ce6cf2525d239c458bab076f0
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-06-24 10:14:54 -0500

    created stat endpoints

--- GameOn/stats/views.py
+++ GameOn/stats/views.py
@@ -9,43 +9,43 @@ from django.http import Http404
 
 
 class StatsList(APIView):
-    pass
-    # def get(self, request):
-    #     pass
-    #     # return Response(serializer.data)
-    #
-    # def post(self, request):
-    #     serializer = StatSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        stats = Stat.objects.all()
+        serializer = StatSerializer(stats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = StatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 
 class StatsDetail(APIView):
-    pass
 
-    # def get_cards(self, pk):
-    #     try:
-    #         return Collection.objects.get(pk=pk)
-    #     except Collection.DoesNotExist:
-    #         raise Http404
-    #
-    # def get_collection(self, pk):
-    #     try:
-    #         return Collection.objects.get(pk=pk)
-    #     except Collection.DoesNotExist:
-    #         raise Http404
-    #
-    # def get(self, pk):
-    #     collection = self.get_collection(pk)
-    #     serializer = UserSerializer(collection)
-    #     return Response(serializer.data)
-    #
+    def get_stats(self, fk):
+        try:
+            return Stat.objects.get(fk=fk, many=True)
+        except Stat.DoesNotExist:
+            raise Http404
+
+    def get_specific_stat(self, pk):
+        try:
+            return Stat.objects.get(pk=pk)
+        except Stat.DoesNotExist:
+            raise Http404
+
+    def get(self, pk):
+        stats = self.get_stats(pk)
+        serializer = StatSerializer(stats)
+        return Response(serializer.data)
+
     # def put(self, request, pk):
     #     pass
     #
-    # def delete(self, request, pk):
-    #     collection = self.get_collection(pk)
-    #     collection.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, pk):
+        stat = self.get_specific_stat(pk)
+        stat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
