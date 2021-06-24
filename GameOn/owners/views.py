59aafa715f378e347cf58868a76fa1b474c39048
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-06-24 14:38:26 -0500

    finished owner endpoints

--- GameOn/owners/views.py
+++ GameOn/owners/views.py
@@ -25,28 +25,26 @@ class OwnersList(APIView):
 
 
 class OwnersDetail(APIView):
-    pass
-    # def get_cards(self, pk):
-    #     try:
-    #         return Collection.objects.get(pk=pk)
-    #     except Collection.DoesNotExist:
-    #         raise Http404
-    #
-    # def get_collection(self, pk):
-    #     try:
-    #         return Collection.objects.get(pk=pk)
-    #     except Collection.DoesNotExist:
-    #         raise Http404
-    #
-    # def get(self, pk):
-    #     collection = self.get_collection(pk)
-    #     serializer = UserSerializer(collection)
-    #     return Response(serializer.data)
-    #
-    # def put(self, request, pk):
-    #     pass
-    #
-    # def delete(self, request, pk):
-    #     collection = self.get_collection(pk)
-    #     collection.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_Owner(self, pk):
+        try:
+            return Owner.objects.get(pk=pk)
+        except Owner.DoesNotExist:
+            raise Http404
+
+    def get(self, pk):
+        owner = self.get_owner(pk)
+        serializer = OwnerSerializer(owner)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        owner = self.get_Owner(pk)
+        serializer = OwnerSerializer(owner, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        owner = self.get_Owner(pk)
+        owner.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
